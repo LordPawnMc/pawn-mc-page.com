3570f790715dbb8e07b3c43b060a202d9133d556
LordPawnMc <142865762+LordPawnMc@users.noreply.github.com>
2023-09-04 19:13:33 +0800

    Delete script.js

--- script.js
+++
@@ -1,9 +0,0 @@
-// JavaScript code can be added here to handle dynamic behavior on your pages.
-// For example, you can use JavaScript to validate the login form, show/hide elements, etc.
-// Below is a simple example of showing an alert on the Home page.
-
-const homePage = document.querySelector('title').textContent === 'Home';
-
-if (homePage) {
-    alert('Warning: This Website Can Harm Your Device Do you Proceed > ; Please Leave!!!');
-}
